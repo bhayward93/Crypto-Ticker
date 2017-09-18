1434e41a3592372b538d72e49fb0bd881076f5e8
bhayward93 <benhayward93@hotmail.com>
2017-09-18 23:19:39 +0100

    Basic structure of the project laid out.

---
+++ js/main.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Ben Hayward on 18/09/17.
+ */
+
+var COIN_FLOOR_URL = "https://api.coindesk.com/v1/bpi/currentprice.json";
+
+setBadgeText(getJSON(COIN_FLOOR_URL));
+function setBadgeText(currentPrice){
+    chrome.browserAction.setBadgeText(currentPrice);
+}
+
+function setBadgeBGColor(){
+    chrome.browserAction.setBadgeBackgroundColor({color: [0,255,0,255]});
+}
+
+function getJSON(url){https://api.coindesk.com/v1/bpi/currentprice.json
+        $.get(url, function(data, status){
+            alert("Data: " + data + "\nStatus: " + status);
+        });
+}
+
