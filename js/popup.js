85abb3d165917aea1b58dac11f76fd2e02bf2d39
bhayward93 <benhayward93@hotmail.com>
2017-09-22 21:49:15 +0100

    Button click works, AJAX Get failing now.
    The problem was that the DOMContentLoaded event listener was being ignored, because it was already loaded by the time the JS was reached.

--- js/popup.js
+++ js/popup.js
@@ -2,29 +2,43 @@
  * Created by Ben Hayward on 19/09/17.
  */
 
-document.addEventListener('DOMContentLoaded', function() { //Adding an event listener to the document
-    var COIN_FLOOR_URL = 'https://api.coindesk.com/v1/bpi/currentprice.json';
-    alert('Document EventListener Loaded');  //Test2
-   $('currentPrice').addEventListener('click', updateClicked() , false);
-}, false);
-//try chromium
+var COIN_FLOOR_URL = 'https://api.coindesk.com/v1/bpi/currentprice.json';
+
+checkReadyStateAndExecute();
+
+
+function checkReadyStateAndExecute(){
+    if(document.readyState === 'loading' ) {
+        addClick();
+    } else  {
+        document.addEventListener('DOMContentLoaded', addClick());
+    }
+}
+
+
+function addClick() {
+    var button = document.getElementById('currentPrice');
+    button.addEventListener('click', updateClicked);
+}
+
 
 function updateClicked(){
-    alert ('11111');
-    var temptxt = getJSON(COIN_FLOOR_URL);
+    var temptxt = getJSON(COIN_FLOOR_URL); //currently not firing.
     setBadgeText(temptxt);
     alert(temptxt);
 }
+
+
+function getJSON(url){  //https://api.coindesk.com/v1/bpi/currentprice.json
+    $.ajax.get(url, function(data, status){
+        alert("Data: " + data + "\nStatus: " + status);
+    });
+}
+
 function setBadgeText(currentPrice){
     chrome.browserAction.setBadgeText(currentPrice);
 }
 
 function setBadgeBGColor(){
     chrome.browserAction.setBadgeBackgroundColor({color: [0,0,255]});
-}
-
-function getJSON(url){  //https://api.coindesk.com/v1/bpi/currentprice.json
-    $.get(url, function(data, status){
-        alert("Data: " + data + "\nStatus: " + status);
-    });
 }
